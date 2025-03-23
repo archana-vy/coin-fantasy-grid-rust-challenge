0fa617d6ce26dc5cea1e070ba5646f29d10ab990
Archana Vaideeswaran <archanav1929@gmail.com>
2025-03-24 00:04:58 +0530

    initialze context

--- proposal-system/programs/proposal-system/src/lib.rs
+++ proposal-system/programs/proposal-system/src/lib.rs
@@ -1,4 +1,5 @@
 use anchor_lang::prelude::*;
+use anchor_lang::solana_program::program_pack::Pack;
 
 #[cfg(feature = "devnet")]
 const PROGRAM_ID: Pubkey = Pubkey::from_str_const("2F3xVyraFXuZpCShqg2YVuF5HxNukmGtGdkm5fJ41L3R");
@@ -15,10 +16,48 @@ pub mod proposal_system {
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+        let multisig_program = &mut ctx.accounts.multisig_program;
+        multisig_program.multisig_accounts = Vec::new();
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(
+        init,
+        payer = signer,
+        space = 8 + 4 + (32 * 100) // 8 bytes (discriminator) + 4 bytes (Vec len) + (Pubkey size * max no. of accounts)
+    )]
+    pub multisig_program: Account<'info, MultisigProgram>,
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+#[derive(Debug)]
+pub struct MultisigProgram {
+    // initializer just to track multisig accounts created using the program
+    pub multisig_accounts: Vec<Pubkey>,
+    // add more parameters if requires
+}
+
+#[account]
+pub struct Multisig {
+    pub name: String,
+    pub creator: Pubkey,
+    pub signers: Vec<Pubkey>,
+    pub threshold: u32,
+    pub proposal_count: u64,
+}
+
+#[account]
+pub struct Proposal {
+    pub multisig: Pubkey,
+    pub creator: Pubkey,
+    pub calldata: Vec<u8>,
+    pub approvals: Vec<Pubkey>,
+    pub executed: bool,
+}
+
